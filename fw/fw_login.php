d163a86b9080b6cd665814a7518087641bf17182
Mohammad Yunus <Mohammad Yunus>
2017-04-27 11:00:23 +0530

    menechanges

--- fw/fw_login.php
+++ fw/fw_login.php
@@ -1,15 +1,11 @@
 <?php
 include("fw_db.php");
-
 session_start(); 
-
 $username=($_POST['username']);
 $password=($_POST['password']);
-
 $result=  $conn->query("SELECT username,password FROM accholder WHERE username='$username' and password='$password'");
-
 if($result->num_rows >0){
-    echo "here";
+    header("Location: menu.php");
 }
 else
     echo "not here";
--- fw/menu.php
+++ fw/menu.php
@@ -10,7 +10,7 @@ echo "Failed to connect to MySQL: " . mysqli_connect_error();
 
 $result = mysqli_query($conn, "SELECT * FROM bf");
 $i = 0;
-echo "<table border='1'>
+echo "<table border='1' id='myTable'>
 <tr>
 <th>Item</th>
 <th>price</th>
@@ -20,11 +20,11 @@ while($row = mysqli_fetch_array($result))
 echo "<tr>";
 echo "<td>" . $row['fname'] . "</td>";
 echo "<td>" . $row['price'] . "</td>";
-echo "<td><input type='checkbox'></td>";   
+echo "<td><input type='button' value='1'></td>";   
 //echo "<td><input type='button' class='cartAdd' id='add" ; echo $i ; echo "' value='+'></td>";
 echo "</tr>";$i++;
 }
-echo "</table>";
+echo "</table><br><input type='button' value='Add' onclick='gg()'>";
         
 
 mysqli_close($conn);
@@ -50,10 +50,24 @@ mysqli_close($conn);
         </table>
         <script>
             function gg(){
-            document.getElementById("s").innerHTML = "i m here";
-        }
-            
-            
+            var oTable = document.getElementById('myTable');
+
+            //gets rows of table
+            var rowLength = oTable.rows.length;
+                
+            //loops through rows    
+                for (i = 1; i < rowLength; i++){
+                  //gets cells of current row  
+                    var oCells = oTable.rows.item(i).cells;
+                    var textnode = document.createTextNode(oCells.item(2).value+ " ");
+                            document.getElementById("s").appendChild(textnode);
+
+                        if(oCells.item(2).checked){
+                            var textnode = document.createTextNode(oCells.item(0).innerHTML);
+                            document.getElementById("s").appendChild(textnode);
+                        }
+                }
+            }
         </script>
         
 </body>
